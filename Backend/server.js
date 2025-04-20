9592a28c45d262a26872fa3ac0b9aeed47d108c1
Amrisingh <amriteshsingh0006@gmail.com>
2025-04-20 10:26:29 -0700

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: "https://official-website-dqdw.vercel.app/", 
+  origin: "https://official-website-dqdw.vercel.app", 
   methods: ["GET", "POST"]
 }));
 app.use(express.json());
