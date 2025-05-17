2c8bddfd0d925155b16e8646816fc161d13741aa
Amrisingh <amriteshsingh0006@gmail.com>
2025-05-17 09:38:48 -0700

    SEO

--- frontend-react/src/components/Howtoorder/Howtoorder.jsx
+++ frontend-react/src/components/Howtoorder/Howtoorder.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import Details1 from './details1/details1'
+import { Helmet } from 'react-helmet';
 
 const Howtoorder = () => {
   return (
     <>
+      <Helmet>
+        <title>How to Order - QuickDabba</title>
+        <meta
+          name="description"
+          content="Find out how to order healthy, homemade meals from QuickDabba. Easy process and fresh food delivered to your doorstep."
+        />
+      </Helmet>
     <Details1 />    
       
     </>
